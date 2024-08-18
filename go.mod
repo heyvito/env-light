d40bed705a2b86d0917ee65905443788a5f6a7da
Vito Sartori <hey@vito.io>
2024-08-18 16:52:57 -0300

    WIP

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/heyvito/env-light
 
 go 1.15
 
-require github.com/brutella/hc v1.2.5 // indirect
+require (
+	github.com/brutella/hc v1.2.5 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/brutella/hc"
 	"github.com/brutella/hc/accessory"
 	"github.com/heyvito/env-light/ws281x"
+	"github.com/lucasb-eyer/go-colorful"
 	"log"
 )
 
@@ -19,16 +20,42 @@ func main() {
 	err := mat.Init(78, 18)
 	fatal(err)
 
+	var H, S, L float64
+
 	info := accessory.Info{Name: "Table Light"}
 	ac := accessory.NewColoredLightbulb(info)
+
+	updateColor := func() {
+		c := colorful.Hsl(H, S/100, L/100)
+		r, g, b := c.RGB255()
+		if err := mat.SetColor(r, g, b, uint8(int(L)*255)); err != nil {
+			fmt.Printf("Error updating color: %s\n", err)
+		}
+	}
+
 	ac.Lightbulb.Hue.OnValueRemoteUpdate(func(f float64) {
-		fmt.Printf("Hue: %f\n", f)
+		H = f // 0-360
+		updateColor()
 	})
+
 	ac.Lightbulb.Saturation.OnValueRemoteUpdate(func(f float64) {
-		fmt.Printf("Saturation: %f\n", f)
+		S = f / 100.0
+		updateColor()
 	})
+
 	ac.Lightbulb.Brightness.OnValueRemoteUpdate(func(i int) {
-		fmt.Printf("Brightness: %d\n", i)
+		L = float64(i) / 100.0
+		updateColor()
+	})
+
+	ac.Lightbulb.On.OnValueRemoteUpdate(func(b bool) {
+		if !b {
+			if err := mat.SetColor(0, 0, 0, 0); err != nil {
+				fmt.Printf("Error setting color: %s\n", err)
+			}
+		} else {
+			updateColor()
+		}
 	})
 
 	// configure the ip transport
